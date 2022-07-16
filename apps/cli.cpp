f23abc7642ed4802a5df6c438ba208a296134f6c
Atlanswer <atlanswer@gmail.com>
2022-07-17 01:11:18 +0800

    Complete vcpkg support

--- apps/cli.cpp
+++ apps/cli.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fmt/core.h>
 
 #include "libName/libName.hpp"
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, world!"
-    << " from libName version " << libName::VERSION << ".\n";
-    return 0;
+int main(int argc, char **argv) {
+  // std::cout << "Hello, world!"
+            // << " from libName version " << libName::VERSION << ".\n";
+  fmt::print("The answer is {}.", 42);
+  return 0;
 }
