ecd8c53f8efa55880c0eef9faca5380062f86902
Atlanswer <atlanswer@gmail.com>
2022-08-25 13:31:53 +0800

    Register doctest test cases in CTest

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,7 +10,7 @@ int factorial(int number) {
 
 TEST_CASE("Testing the factorial function") {
     CHECK(factorial(0) == 1);
-    CHECK(factorial(1) == 1);
+    CHECK(factorial(1) == 2);
     CHECK(factorial(2) == 2);
     CHECK(factorial(3) == 6);
     CHECK(factorial(10) == 3628800);
